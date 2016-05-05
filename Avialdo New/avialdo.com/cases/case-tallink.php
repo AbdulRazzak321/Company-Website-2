4619436c81e601e2f6503d2611991ad2196c13d8
AbdulRazzak321 <a.razzaqbhutto@yahoo.com>
2016-05-05 13:55:22 +0500

    changed

---
+++ Avialdo New/avialdo.com/cases/case-tallink.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<head prefix="og: http://ogp.me/ns#">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0"/>
+
+  <title>Digital capabilities and innovation culture for a ferry company - Tallink — Futurice</title>
+  <link rel="icon" type="image/png" href="../../static.flockler.com/assets/futurice/images/favicon-73ef94531581d9f27e41233515c60919.png">
+  <meta property="og:title" content="Digital capabilities and innovation culture for a ferry company - Tallink — Futurice">
+  <meta property="og:description" name="description" content="Developing digital capabilities and innovation culture for a ferry company ">
+  <meta property="og:type" content="article">
+  <meta property="og:url" content="case-tallink.html">
+  <meta property="og:image" content="../assets/images/res/small2015_ts_ships_baltic_queen_night_jokemedia_rgb_007_s1200x630_q80_noupscale.jpg">
+  <meta property="og:image:width" content="1200">
+  <meta property="og:image:height" content="630">
+</head>
+<body class="view--articles view--articles-show is-article-view is-case-study-view" itemscope itemtype="http://schema.org/WebPage">
+<?php
+$current = 'work';
+include ("../Header.php");
+    ?>
+  <header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
+  <div class="header__container">
+    <img alt="Bg-1" onerror="this.onerror=null;this.src='../assets/futurice/images/backgrounds/bg-1.png';" src="http://static.flockler.com/assets/futurice/images/backgrounds/bg-1.png" />
+
+  </div>
+</header>
+
+  <main class="main" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/WebPage">
+    
+  <div class="article-wrapper">
+  <article class="article article--case" itemscope itemprop="article" itemtype="http://schema.org/Article">
+    <header class="article__header">
+      <div class="article__header__meta">
+          <span class="article__header__meta__category" itemprop="articleSection">
+            Consulting
+          </span>
+      </div>
+      <h1 class="article__header__title" itemprop="headline">
+        Digital capabilities and innovation culture for a ferry company - WalknSell
+      </h1>
+    </header>
+
+
+    <div class="article__body" itemprop="articleBody">
+      <figure class="article__figure">
+<div class="article__figure__placeholder"><img alt="source: AS Tallink Grupp​" src="../assets/images/work/walknsell.png"></div>
+
+<figcaption><span class="article__figure__caption">source: AS Tallink Grupp​</span></figcaption>
+</figure>
+
+<p>Tallink is the market leader in leisure and business travel in the Baltic Sea area, with a 46% market share of the passenger market. Over 8.8 million passengers traveled on Tallink ferries in 2014. Based on revenues, it is the third largest ferry operator in the world. On-board sales are an important part of the business, comprising 50% of revenues. Tallink also provides hotel, travel, cargo and transportation services. </p>
+
+<p>More info on Tallink: <a href="http://www.tallink.com/" target="_blank">www.tallink.com</a> </p>
+
+<p><strong>Problem statement</strong></p>
+
+<p>The Tallink management board wanted an independent review of the current state of digital development in their organisation. They wanted to identify ways to speed up projects and set up innovation processes to meet business needs - all necessary to build the digital capabilities that enable a better customer experience and personalization of the offering, while providing up &amp; cross-sell opportunities as well as optimizing costs through self-service.</p>
+
+<p>Tallink’s software development environment is quite complex. Several, often physically separated teams work on development projects. Project timelines and change management are critical challenges. Finding the best solutions to meet both IT and business needs is not easy, either.</p>
+
+<figure class="article__figure">
+<div class="article__figure__placeholder"><img alt="source: AS Tallink Grupp​" src="../assets/images/res/small_ts_seaview_002--tphy9m1sjx_s830x0_q80_noupscale.jpg"></div>
+
+<figcaption><span class="article__figure__caption">source: AS Tallink Grupp​</span></figcaption>
+</figure>
+
+<p>For an organisation to respond to a changing business environment, it must change the way it works. New problems require novel solutions.</p>
+
+<p>What Tallink had was a strong desire to learn and change, and an ambition to be a pioneer in their industry. What they needed was a better understanding of the current state of their organisation, as well as the tools to improve it.</p>
+
+<p><strong>Approach</strong></p>
+
+<p>A three-person core team from Futurice, representing the technology, design, business competences, worked on-site in Tallinn, Estonia.  </p>
+
+<ul>
+	<li>Project duration: 7 weeks</li>
+	<li>Interviews with 28 people across the organization</li>
+	<li>Three projects in more detail</li>
+	<li>Value stream mapping workshops with project teams</li>
+	<li>Technology landscape review</li>
+</ul>
+
+<p>All major stakeholders were involved. Various aspects of the innovation process were covered, including organisation, culture, technologies, tools, and partners.</p>
+
+<div>
+<p><strong>Impact</strong></p>
+</div>
+
+<p>Tallink has very skilled and motivated people who can transform the company and establish it as a benchmark for utilizing digital in the travel industry. To unleash the potential and create an environment that enables Tallink to succeed in this, we worked on the following assets:</p>
+
+<ul>
+	<li>Sufficient understanding for starting a digital change program</li>
+	<li>Perspective from a partner with experience delivering successful projects</li>
+	<li>Analysis and documentation of the current state</li>
+	<li>Organization and project level recommendations</li>
+	<li>Engagement from both management and project teams</li>
+</ul>
+
+<p><em>“The team from Futurice was professional in conducting the analysis and produced results that can help us speed up our digital development.” </em><br>
+<strong>Janek Stalmeister, CEO, Tallink Group</strong></p>
+
+    </div>
+
+
+    <div class="contact-persons">
+      <h3 class="contact-person__heading">For more information, contact:</h3>
+        <div class="contact-person" itemscope itemptype="http://schema.org/Person">
+          <figure class="contact-person__figure">
+            <img alt="" src="../assets/images/res/sampo2--u3thoqmf4l_s100x100_q80_noupscale.jpg" />
+          </figure>
+          <div class="contact-person__body">
+            <span class="contact-person__name" itemprop="name">Sampo Hämäläinen</span>
+            <span class="contact-person__title" itemprop="jobTitle"></span>
+            <div class="contact-person__details">
+                <span class="contact-person__email">
+                  <i class="icon-mail"></i>
+                  <span itemprop="email">sampo@futurice.com</span>
+                </span>
+                <span class="contact-person__phone">
+                  <i class="icon-phone"></i>
+                  <span itemprop="telephone">+35840 511 2299</span>
+                </span>
+            </div>
+          </div>
+        </div>
+        <div class="contact-person" itemscope itemptype="http://schema.org/Person">
+          <figure class="contact-person__figure">
+            <img alt="" src="../assets/images/res/mari-piirainen_s100x100_q80_noupscale.jpg" />
+          </figure>
+          <div class="contact-person__body">
+            <span class="contact-person__name" itemprop="name">Mari Piirainen</span>
+            <span class="contact-person__title" itemprop="jobTitle">Senior Consultant</span>
+            <div class="contact-person__details">
+                <span class="contact-person__email">
+                  <i class="icon-mail"></i>
+                  <span itemprop="email">mari.piirainen@futurice.com</span>
+                </span>
+                <span class="contact-person__phone">
+                  <i class="icon-phone"></i>
+                  <span itemprop="telephone">+358 50  487 2102</span>
+                </span>
+            </div>
+          </div>
+        </div>
+    </div>
+
+    <footer class="article__footer">
+      <span class="article__footer__item article__footer__item--share">
+  <span class="article__footer__item__label">Share</span>
+  <div class="article__footer__item__body">
+
+    <a onClick="event.preventDefault(); window.open('http://www.facebook.com/sharer.php?u=http%3A%2F%2Ffuturice.com%2Fcases%2Fcase-tallink', 'sharer', 'toolbar=0,status=0,width=548,height=325');" target="_blank"  class="article__footer__item__share-link" href="http://www.facebook.com/sharer.php?u=http%3A%2F%2Ffuturice.com%2Fcases%2Fcase-tallink">
+      <i class="icon-facebook"></i>
+    </a>
+
+    <a onClick="event.preventDefault(); window.open('https://twitter.com/intent/tweet?text=Digital%20capabilities%20and%20innovation%20culture%20for%20a%20ferry%20company%20-%20Tallink&amp;url=http%3A%2F%2Ffuturice.com%2Fcases%2Fcase-tallink', 'sharer', 'toolbar=0,status=0,width=548,height=525');" target="_blank" class="article__footer__item__share-link"
+    href="https://twitter.com/intent/tweet?text=Digital%20capabilities%20and%20innovation%20culture%20for%20a%20ferry%20company%20-%20Tallink&amp;url=http%3A%2F%2Ffuturice.com%2Fcases%2Fcase-tallink">
+      <i class="icon-twitter"></i>
+    </a>
+
+    <a onClick="event.preventDefault(); window.open('https://plus.google.com/share?url=http%3A%2F%2Ffuturice.com%2Fcases%2Fcase-tallink', 'sharer', 'toolbar=0,status=0,width=548,height=525');" target="_blank" class="article__footer__item__share-link"
+    href="https://plus.google.com/share?url=http%3A%2F%2Ffuturice.com%2Fcases%2Fcase-tallink">
+      <i class="icon-google-plus"></i>
+    </a>
+
+    <a onClick="event.preventDefault(); window.open('https://www.linkedin.com/shareArticle?mini=true&amp;url=http%3A%2F%2Ffuturice.com%2Fcases%2Fcase-tallink&amp;title=Digital%20capabilities%20and%20innovation%20culture%20for%20a%20ferry%20company%20-%20Tallink', 'sharer', 'toolbar=0,status=0,width=548,height=525');" target="_blank" class="article__footer__item__share-link"
+    href="https://www.linkedin.com/shareArticle?mini=true&amp;url=http%3A%2F%2Ffuturice.com%2Fcases%2Fcase-tallink&amp;title=Digital%20capabilities%20and%20innovation%20culture%20for%20a%20ferry%20company%20-%20Tallink">
+      <i class="icon-linkedin"></i>
+    </a>
+  </div>
+</span>
+
+    </footer>
+  </article>
+</div>
+
+
+  </main>
+  
+<?php
+include ("../footer.php");
+    ?>
+</body>
+
+</html>
